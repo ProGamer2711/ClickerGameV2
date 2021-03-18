0ad6a875e3aaba3de406dcd4c38c2b20979f5586
Alexander Shestakov <alexandurshestakov@gmail.com>
2021-03-18 19:57:17 +0200

    Added database config and security

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+    return [
+        "database" => [
+            "hostname" => "127.0.0.1",
+            "username" => "root",
+            "password" => "",
+            "database" => "clickergamev2",
+            "port" => "3306"
+        ]
+    ];
+
+?>
--- db_connection.php
+++ db_connection.php
@@ -1,13 +1,11 @@
 <?php
 
-    $hostname = '127.0.0.1';
-    $username = 'root';
-    $password = '';
-    $database = 'clickergamev2';
+    $config = require('config.php');
+    $config = $config['database'];
 
-    $mysqli = new mysqli($hostname, $username, $password, $database);
+    $mysqli = new mysqli($config['hostname'], $config['username'], $config['password'], $config['database'], $config['port']);
     
     if($mysqli->connect_error){
         die('Connection failed: '.$mysqli->connect_error);
-    }
+    } 
 ?>
