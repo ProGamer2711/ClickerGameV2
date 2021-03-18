2ae7dc48d007ff481ec4e9401fe39c798beaeca6
Alexander Shestakov <alexandurshestakov@gmail.com>
2021-03-18 20:10:14 +0200

    Changed config to json file instead of a php file

--- db_connection.php
+++ db_connection.php
@@ -1,9 +1,11 @@
 <?php
 
-    $config = require('config.php');
-    $config = $config['database'];
+    $config = fopen('./config.json', 'r') or die('File "config.json" not found.');
+    $text = fread($config, filesize('./config.json'));
+    $json = json_decode($text);
+    $json_db = $json->database;
 
-    $mysqli = new mysqli($config['hostname'], $config['username'], $config['password'], $config['database'], $config['port']);
+    $mysqli = new mysqli($json_db->hostname, $json_db->username, $json_db->password, $json_db->database, $json_db->port);
     
     if($mysqli->connect_error){
         die('Connection failed: '.$mysqli->connect_error);
